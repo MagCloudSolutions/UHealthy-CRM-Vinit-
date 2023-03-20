648df24ead06f36cfcd40ef2f1e6617355a89fe5
corbinbot <viveklawaniya98@gmail.com>
2023-03-21 01:04:55 +0530

    c

--- data.php
+++ data.php
@@ -21,7 +21,7 @@ session_start();
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Bootstrap demo</title>
+        <title>Data</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <link rel="stylesheet" href="homestyle.css">
@@ -124,8 +124,9 @@ session_start();
                   Select
                 </button>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Update</a></li>
-                  <li><a class="dropdown-item" href="#">Delete</a></li>
+                  <li><form action="update.php" method="get"><input style="display:none" name="s_no" value=' . $row["s_no"] . '><button class="dropdown-item" type="submit" >Update</button></form></li>
+                  <li><form action="delete.php" method="get"><input style="display:none" name="s_no" value=' . $row["s_no"] . '><button class="dropdown-item" type="submit" >Delete</button></form></li>
+                  
                 </ul>
               </div>';
                     // echo "<div class='dropdown show'>";
@@ -150,6 +151,9 @@ session_start();
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
             </script>
+        <script src="https://code.jquery.com/jquery-3.6.4.js"
+            integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
+        <script src="index.js"></script>
     </body>
 
 </html>
