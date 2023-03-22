5a7eed9590a4760ba0364673f62d7841166f9298
corbinbot <viveklawaniya98@gmail.com>
2023-03-22 12:09:12 +0530

    c

--- update.php
+++ update.php
@@ -44,46 +44,46 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="homestyle.css">
-    <title>form</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="homestyle.css">
+        <title>form</title>
+    </head>
 
-<body>
-    <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-            <!-- <a class="navbar-brand" href="#">Navbar</a> -->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="home.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="data.php">Data</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="Logout.php">Logout</a>
-                    </li>
+    <body>
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="data.php">Data</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Logout.php">Logout</a>
+                        </li>
 
 
-                </ul>
+                    </ul>
 
+                </div>
             </div>
-        </div>
-    </nav>
-    <div class="wrapper">
-        <div class="title">User Details</div>
-        <?php
+        </nav>
+        <div class="wrapper">
+            <div class="title">User Details</div>
+            <?php
         if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             include "connection.php";
             $id = $_GET['s_no'];
@@ -99,15 +99,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         <div class="input-box">
                             <span class="details">Name</span>
                             <input style="display:none" name="s_no" type="text" value="' . $id . '" placeholder="Enter your name" required>
-                            <input name="name" type="text" value="' . $row['name'] . '" placeholder="Enter your name" required>
+                            <input name="name" type="text" value="' . $row['name'] . '" placeholder="Enter your name" disabled required>
                         </div>
                         <div class="input-box">
                             <span class="details">Phone Number</span>
-                            <input name="number" type="number" value="' . $row['phone'] . '" placeholder="Enter your numer" required>
+                            <input name="number" type="number" value="' . $row['phone'] . '" placeholder="Enter your numer" disabled required>
                         </div>
                         <div class="input-box">
                             <span class="details">E-Mail</span>
-                            <input name="email" type="text" value="' . $row['email'] . '" placeholder="Enter your email" required>
+                            <input name="email" type="text" value="' . $row['email'] . '" placeholder="Enter your email" disabled required>
                         </div>
                         <div class="input-box">
                             <span class="details">State</span>
@@ -194,10 +194,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
         } ?>
 
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
         </script>
-</body>
+    </body>
 
 </html>
