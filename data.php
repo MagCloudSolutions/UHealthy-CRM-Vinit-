c654dcd4641b3dfe024869c5f9e7f5090faf1ff1
poojaraheja <rahejapooja670@gmal.com>
2023-03-23 22:57:29 +0530

    code

--- data.php
+++ data.php
@@ -52,8 +52,8 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
     </nav>
     <div class="">
         <form class="search" action="data.php" method="post">
-            <input type="text" id="search" name="search" style="margin: 10px;padding:10px;border-radius:10px;width:100%"
-                required>
+            <input type="text" id="search" name="search"
+                style="margin: 10px;padding:10px;border-radius:10px;width:100%">
             <div class="input-group date_search" id="date_search" style="margin: 10px;padding:10px;border-radius:10px;width:100%>
                 <span className=" input-group-text">From :</span>
                 <input type="date" name="from" id="date1" className="btn btn-outline-dark
@@ -160,6 +160,8 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
 
             } elseif ($search != "" && $select != "") {
                 $sql = "SELECT * FROM data WHERE $select LIKE '{$search}%'";
+            } else {
+                echo "Data Not Found";
             }
 
 
@@ -236,7 +238,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
-        </script>
+    </script>
     <script src="https://code.jquery.com/jquery-3.6.4.js"
         integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
     <script src="js/index.js"></script>
