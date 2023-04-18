3764d81040e704a38b2515e4be4a561bce2a5331
corbinbot <viveklawaniya98@gmail.com>
2023-04-18 17:02:19 +0530

    c

--- update.php
+++ update.php
@@ -52,57 +52,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/homestyle.css">
-    <title>form</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/homestyle.css">
+        <title>form</title>
+    </head>
 
-<body>
-    <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-            <!-- <a class="navbar-brand" href="#">Navbar</a> -->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="home.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="data.php">Data</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="Logout.php">Logout</a>
-                    </li>
-
-
-                </ul>
-
-            </div>
-        </div>
-    </nav>
-    <div class="wrapper">
-        <div class="title">User Details</div>
+    <body>
         <?php
-        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-            include "connection.php";
-            $id = $_GET['s_no'];
+        include 'navbar.php';
+        ?>
+        <div class="wrapper">
+            <div class="title">User Details</div>
+            <?php
+            if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+                include "connection.php";
+                $id = $_GET['s_no'];
 
-            $sql = "SELECT * from data where s_no = '$id'";
-            $result = mysqli_query($conn, $sql);
-            $num = mysqli_num_rows($result);
-            $row = mysqli_fetch_assoc($result);
-            if ($result) {
-                // echo "<script>alert('" . $row['name'] . "')</script>";
-                echo '<form action="update.php" method="post">
+                $sql = "SELECT * from data where s_no = '$id'";
+                $result = mysqli_query($conn, $sql);
+                $num = mysqli_num_rows($result);
+                $row = mysqli_fetch_assoc($result);
+                if ($result) {
+                    // echo "<script>alert('" . $row['name'] . "')</script>";
+                    echo '<form action="update.php" method="post">
                     <div class=" user-details">
                         <div class="input-box">
                             <span class="details">Name</span>
@@ -201,16 +178,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         <input type="submit" value="Update Your Details">
                     </div>
                 </form>';
-            } else {
-                echo "<script>alert('Error')</script>";
+                } else {
+                    echo "<script>alert('Error')</script>";
 
-            }
-        } ?>
+                }
+            } ?>
 
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
         </script>
-</body>
+    </body>
 
 </html>
