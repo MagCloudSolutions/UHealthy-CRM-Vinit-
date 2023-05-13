ff31f4e08fa4befd8e451c1a0b9df84ff419b899
corbinbot <viveklawaniya98@gmail.com>
2023-05-13 14:10:46 +0530

    c

--- database/freelancer_php.sql
+++ database/freelancer_php.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 22, 2023 at 09:40 AM
+-- Generation Time: May 13, 2023 at 10:39 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -57,18 +57,32 @@ CREATE TABLE `data` (
   `cusomer_disc` varchar(1000) NOT NULL,
   `home_listing` varchar(255) NOT NULL,
   `retailer_id` int(255) NOT NULL,
-  `created_at` date NOT NULL DEFAULT current_timestamp()
+  `id` int(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `data`
 --
 
-INSERT INTO `data` (`s_no`, `name`, `phone`, `email`, `state`, `gender`, `age`, `height`, `weight`, `looking_for`, `attended_dc`, `exp_date_dc`, `attended_mhf`, `exp_date_mhf`, `current_status`, `lead_date`, `comments`, `assigned_to`, `followup_date`, `is_organic`, `adset_name`, `campaign`, `vehicle`, `partner`, `platform`, `lead_id`, `cusomer_disc`, `home_listing`, `retailer_id`, `created_at`) VALUES
-(2, 'Vinit', 589562, 'vhuyhjn8@gmail.com', 'on', 'male', 65, 5, 5, 'Healthy Weight Loss', 'no', '2023-03-03', 'no', '2023-03-23', 'Paid & Joined Club', '2023-03-10', 'bt', '', '0000-00-00', '', '', '', '', '', '', '', '', '', 0, '2023-03-20'),
-(5, 'TEst', 486512, 'jhuij8@gmail.com', 'Uttar Pradesh', 'on', 65, 25, 36, 'ntjnnk', 'yes', '2023-03-01', 'no', '2023-03-29', 'none', '2023-03-31', 'ok', '', '0000-00-00', '', '', '', '', '', '', '', '', '', 0, '2023-03-20'),
-(6, 'TEst DAta', 5995959552, 'ngtybjknik@gmail.com', 'Uttar Pradesh', 'female', 55, 162, 54, 'Healthy Weight Gain', 'yes', '2023-03-14', 'no', '2023-03-17', 'Paid & Joined Club', '2023-03-22', 'none ', '', '0000-00-00', '', '', '', '', '', '', '', '', '', 0, '2023-03-20'),
-(7, 'Vivek Lawaniy', 999765565, 'gvbh8@gmai.com', 'onbh', 'malenj', 655, 55, 56, 'Healthy Weight Losshbbj', 'no nhby', '2023-03-08', 'nogty', '2023-03-07', 'Paid & Joined Clubvfg', '2023-03-01', 'btb gvtyg', '', '0000-00-00', '', '', '', '', '', '', '', '', '', 0, '2023-03-21');
+INSERT INTO `data` (`s_no`, `name`, `phone`, `email`, `state`, `gender`, `age`, `height`, `weight`, `looking_for`, `attended_dc`, `exp_date_dc`, `attended_mhf`, `exp_date_mhf`, `current_status`, `lead_date`, `comments`, `assigned_to`, `followup_date`, `is_organic`, `adset_name`, `campaign`, `vehicle`, `partner`, `platform`, `lead_id`, `cusomer_disc`, `home_listing`, `retailer_id`, `id`) VALUES
+(53, 'TEst DAta', 5995959552, 'ngtybjknik@gmail.com', 'Uttar Pradesh', 'Female', 55, 162, 54, 'Healthy Weight Gain', 'yes', '2023-03-14', 'no', '2023-03-17', 'Call Not Picked', '2023-03-22', 'none ', 'Pooja Raheja', '2023-05-12', '', '', '', '', '', '', '', '', '', 0, 0),
+(55, 'MMA', 946154615, 'cnwc@bvrnj.coe', 'Uttar Pradesh', 'Male', 20, 25, 36, 'Healthy Weight Loss', 'no', '2023-03-01', 'no', '2023-03-29', 'Paid & Joined Club', '2023-03-31', 'ok', 'Vivek Lawaniya', '0000-00-00', '', '', '', '', '', '', '', '', '', 0, 4),
+(57, 'Vivek Lawaniy', 999765565, 'gvbh8@gmai.com', 'onbh', 'Male', 655, 55, 56, 'Healthy Weight Loss', 'No', '2023-03-08', 'No', '2023-03-07', 'Interested but did not pay', '2023-03-01', 'btb gvtyg', 'Pooja Raheja', '2023-05-11', '', '', '', '', '', '', '', '', '', 0, 3),
+(73, 'TEst DAta', 5995959552, 'ngtybjknik@gmail.com', 'Uttar Pradesh', 'Female', 55, 162, 54, 'Healthy Weight Gain', 'yes', '2023-03-14', 'no', '2023-03-17', 'Not Interested', '2023-03-22', 'none ', 'Pooja Raheja', '0000-00-00', '', '', '', '', '', '', '', '', '', 0, 3),
+(75, 'MMA', 946154615, 'cnwc@bvrnj.coe', 'Uttar Pradesh', 'Male', 20, 25, 36, 'Healthy Weight Loss', 'no', '2023-03-01', 'no', '2023-03-29', 'Paid & Joined Club', '2023-03-31', 'ok', 'Vivek Lawaniya', '0000-00-00', '', '', '', '', '', '', '', '', '', 0, 4),
+(76, 'TEst DAta', 5995959552, 'ngtybjknik@gmail.com', 'Uttar Pradesh', 'Female', 55, 162, 54, 'Healthy Weight Gain', 'yes', '2023-03-14', 'no', '2023-03-17', 'Paid & Joined Club', '2023-03-22', 'none ', 'caller', '0000-00-00', '', '', '', '', '', '', '', '', '', 0, 2),
+(78, 'TEst DAta', 5995959552, 'ngtybjknik@gmail.com', 'Uttar Pradesh', 'Female', 55, 162, 54, 'Healthy Weight Gain', 'yes', '2023-03-14', 'no', '2023-03-17', 'Not Interested', '2023-03-22', 'none ', 'Pooja Raheja', '0000-00-00', '', '', '', '', '', '', '', '', '', 0, 3),
+(79, 'MMA', 946154615, 'cnwc@bvrnj.coe', 'Uttar Pradesh', 'Male', 20, 25, 36, 'Healthy Weight Loss', 'no', '2023-03-01', 'no', '2023-03-29', 'Paid & Joined Club', '2023-03-31', 'ok', 'Vivek Lawaniya', '0000-00-00', '', '', '', '', '', '', '', '', '', 0, 4),
+(80, 'Vivek Lawaniy', 999765565, 'gvbh8@gmai.com', 'onbh', 'Male', 655, 55, 56, 'Healthy Weight Loss', 'No', '2023-03-08', 'No', '2023-03-07', 'Interested but did not pay', '2023-03-01', 'btb gvtyg', 'Pooja Raheja', '2023-05-11', '', '', '', '', '', '', '', '', '', 0, 3),
+(81, 'TEst DAta', 5995959552, 'ngtybjknik@gmail.com', 'Uttar Pradesh', 'Female', 55, 162, 54, 'Healthy Weight Gain', 'yes', '2023-03-14', 'no', '2023-03-17', 'Not Interested', '2023-03-22', 'none ', 'Pooja Raheja', '0000-00-00', '', '', '', '', '', '', '', '', '', 0, 3),
+(82, 'MMA', 946154615, 'cnwc@bvrnj.coe', 'Uttar Pradesh', 'Male', 20, 25, 36, 'Healthy Weight Loss', 'no', '2023-03-01', 'no', '2023-03-29', 'Paid & Joined Club', '2023-03-31', 'ok', 'Vivek Lawaniya', '0000-00-00', '', '', '', '', '', '', '', '', '', 0, 4),
+(83, 'TEst DAta', 5995959552, 'ngtybjknik@gmail.com', 'Uttar Pradesh', 'Female', 55, 162, 54, 'Healthy Weight Gain', 'yes', '2023-03-14', 'no', '2023-03-17', 'Paid & Joined Club', '2023-03-22', 'none ', 'caller', '0000-00-00', '', '', '', '', '', '', '', '', '', 0, 2),
+(84, 'TEst DAta', 5995959552, 'ngtybjknik@gmail.com', 'Uttar Pradesh', 'Female', 55, 162, 54, 'Healthy Weight Gain', 'yes', '2023-03-14', 'no', '2023-03-17', 'Call Not Picked', '2023-03-22', 'none ', 'Pooja Raheja', '2023-05-12', '', '', '', '', '', '', '', '', '', 0, 0),
+(85, 'Vivek Lawaniy', 999765565, 'gvbh8@gmai.com', 'onbh', 'Male', 655, 55, 56, 'Healthy Weight Loss', 'No', '2023-03-08', 'No', '2023-03-07', 'Interested but did not pay', '2023-03-01', 'btb gvtyg', 'Pooja Raheja', '2023-05-11', '', '', '', '', '', '', '', '', '', 0, 0),
+(86, 'TEst DAta', 5995959552, 'ngtybjknik@gmail.com', 'Uttar Pradesh', 'Female', 55, 162, 54, 'Healthy Weight Gain', 'yes', '2023-03-14', 'no', '2023-03-17', 'Not Interested', '2023-03-22', 'none ', 'Pooja Raheja', '0000-00-00', '', '', '', '', '', '', '', '', '', 0, 0),
+(87, 'TEst DAta', 5995959552, 'ngtybjknik@gmail.com', 'Uttar Pradesh', 'Female', 55, 162, 54, 'Healthy Weight Gain', 'yes', '2023-03-14', 'no', '2023-03-17', 'Not Interested', '2023-03-22', 'none ', 'Pooja Raheja', '0000-00-00', '', '', '', '', '', '', '', '', '', 0, 0),
+(88, 'Vivek Lawaniy', 999765565, 'gvbh8@gmai.com', 'onbh', 'Male', 655, 55, 56, 'Healthy Weight Loss', 'No', '2023-03-08', 'No', '2023-03-07', 'Interested but did not pay', '2023-03-01', 'btb gvtyg', 'Pooja Raheja', '2023-05-11', '', '', '', '', '', '', '', '', '', 0, 0),
+(89, 'TEst DAta', 5995959552, 'ngtybjknik@gmail.com', 'Uttar Pradesh', 'Female', 55, 162, 54, 'Healthy Weight Gain', 'yes', '2023-03-14', 'no', '2023-03-17', 'Not Interested', '2023-03-22', 'none ', 'Pooja Raheja', '0000-00-00', '', '', '', '', '', '', '', '', '', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -81,15 +95,19 @@ CREATE TABLE `user` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `date` date NOT NULL,
-  `active` int(25) NOT NULL
+  `admin` int(25) NOT NULL,
+  `caller` int(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`s_no`, `username`, `password`, `date`, `active`) VALUES
-(1, 'test', 'test', '2023-03-20', 1);
+INSERT INTO `user` (`s_no`, `username`, `password`, `date`, `admin`, `caller`) VALUES
+(1, 'test', 'test', '2023-03-20', 1, 0),
+(2, 'caller', 'caller', '2023-05-09', 0, 1),
+(3, 'Pooja Raheja', 'pooja', '2023-05-09', 0, 1),
+(4, 'Vivek Lawaniya', 'vivek', '2023-05-09', 0, 1);
 
 --
 -- Indexes for dumped tables
@@ -115,13 +133,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `data`
 --
 ALTER TABLE `data`
-  MODIFY `s_no` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `s_no` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `s_no` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `s_no` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
